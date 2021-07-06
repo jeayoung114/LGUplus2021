072cdf3efd582ec610cdb85596664576c6b89d97
psm1206 <psmin1206@gmail.com>
2021-07-06 17:45:56 +0900

    add detail comments

--- models/SLIM_implicit.py
+++ models/SLIM_implicit.py
@@ -18,11 +18,11 @@ class SLIM_implicit():
         self.num_epochs = num_epochs
         self.topk = topk
 
-        alpha = l1_reg + l2_reg
-        l1_ratio = l1_reg / alpha
+        alpha = l1_reg + l2_reg # l1, l2 규제 계수의 합
+        l1_ratio = l1_reg / alpha # alpha를 이용해 l1 비율 계산
         self.slim = ElasticNet(alpha=alpha,
                                 l1_ratio=l1_ratio,
-                                positive=True, # weight matrix has non-negative values
+                                positive=True, # 가중치 행렬이 양수만 갖음
                                 fit_intercept=False,
                                 copy_X=False,
                                 precompute=True,
@@ -42,40 +42,40 @@ class SLIM_implicit():
 
         tqdm_iterator = tqdm(range(self.num_items), desc='# items covered', total=self.num_items)
         for item in tqdm_iterator:
-            # j-th column of R (ground-truth)
+            # 특정 영화의 암시적 피드백 (ground-truth)
             y = train_matrix[:, item].toarray()
-
-            # set the j-th column of R to zero
+            # 사용자-항목 행렬에서 특정 영화의 시작, 끝 포인터
             start_pos = train_matrix.indptr[item]
             end_pos = train_matrix.indptr[item + 1]
-
+            # 특정 영화의 피드백 값을 포인터로 인덱싱하여 따로 백업
             current_item_data_backup = train_matrix.data[start_pos: end_pos].copy()
+            # 사용자-항목 행렬에서 특정 영화에 대해 0으로 채워주면 가중치 행렬에 0이 들어간 효과와 같음
             train_matrix.data[start_pos: end_pos] = 0.0
-
-            # train SLIM
+            # .fit 함수를 이용해 SLIM 학습
             self.slim.fit(train_matrix, y)
 
-            nonzero_model_coef_index = self.slim.sparse_coef_.indices
-            nonzero_model_coef_value = self.slim.sparse_coef_.data
+            nonzero_model_coef_index = self.slim.sparse_coef_.indices # 가중치 행렬의 인덱스
+            nonzero_model_coef_value = self.slim.sparse_coef_.data # 가중치 행렬의 값
 
             for row_index, value in zip(nonzero_model_coef_index, nonzero_model_coef_value):
+                # rows array에 평점이 가득 차면, 빈 행렬을 새로 추가 (기본값: 1000만)
                 if numCells == len(rows):
                     rows = np.concatenate((rows, np.zeros(num_blocks, dtype=np.int32)))
                     cols = np.concatenate((cols, np.zeros(num_blocks, dtype=np.int32)))
                     values = np.concatenate((values, np.zeros(num_blocks, dtype=np.float32)))
-
+                # 해당하는 행, 열 인덱스와 값을 각 array에 저장
                 rows[numCells] = row_index
                 cols[numCells] = item
                 values[numCells] = value
-
+                # 평점이 추가될 때마다 numCells +1
                 numCells += 1
-            
+            # 따로 저장했던 특정 영화의 피드백 값을 다시 복원
             train_matrix.data[start_pos:end_pos] = current_item_data_backup
         
-        # make sparse W matrix
+        # 희소 W 행렬 생성
         self.W_sparse = sp.csr_matrix((values[:numCells], (rows[:numCells], cols[:numCells])), \
                                     shape=(self.num_items, self.num_items), dtype=np.float32)
-
+        # 사용자-항목 행렬과 희소 W 행렬의 행렬 곱을 통해 예측 값 행렬 생성
         self.reconstructed = (train_matrix.tocsr() @ self.W_sparse).toarray()
 
 
