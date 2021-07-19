e1751102766df1440a320d070b13794fd983072a
jaewan7599 <jaewan7599@skku.edu>
2021-07-19 18:52:02 +0900

    add stacking

--- models/DCNV2_implicit.py
+++ models/DCNV2_implicit.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 # https://github.com/rixwew/pytorch-fm/blob/master/torchfm/model/dcn.py을 참고했습니다.
 class DCNV2_implicit(torch.nn.Module):
     def __init__(self, train_data, train_label, valid_data, valid_label, field_dims, embed_dim, num_layers, mlp_dims, dropout,
-                 num_epochs, early_stop_trial, learning_rate, reg_lambda, batch_size, device):
+                 num_epochs, early_stop_trial, learning_rate, reg_lambda, batch_size, device, num_experts, use_stacking=False):
         super().__init__()
 
         self.train_data = train_data
@@ -23,7 +23,6 @@ class DCNV2_implicit(torch.nn.Module):
         self.valid_label = valid_label
         self.field_dims = field_dims
         self.embed_dim = embed_dim
-        self.num_layers = num_layers
         self.mlp_dims = mlp_dims
         self.dropout = dropout
 
@@ -33,6 +32,10 @@ class DCNV2_implicit(torch.nn.Module):
         self.reg_lambda = reg_lambda
         self.batch_size = batch_size
 
+        self.num_layers = num_layers
+        self.num_experts = num_experts
+        self.use_stacking = use_stacking
+
         self.device = device
 
         self.build_graph()
@@ -40,8 +43,12 @@ class DCNV2_implicit(torch.nn.Module):
     def build_graph(self):
         self.embedding = FeaturesEmbedding(self.field_dims, self.embed_dim)
         self.embed_output_dim = len(self.field_dims) * self.embed_dim
-        self.cn = CrossNetworkMMoE(self.embed_output_dim, self.num_layers)
-        self.mlp = MultiLayerPerceptron(self.embed_output_dim, self.mlp_dims, self.dropout, output_layer=False)
+        self.cn = CrossNetworkMMoE(self.embed_output_dim, self.num_layers, num_experts=self.num_experts)
+        if self.use_stacking:
+            output_layer=True
+        else:
+            output_layer=False
+        self.mlp = MultiLayerPerceptron(self.embed_output_dim, self.mlp_dims, self.dropout, output_layer=output_layer)
         self.linear = torch.nn.Linear(self.mlp_dims[-1] + self.embed_output_dim, 1)
 
         # 최적화 방법 설정
@@ -56,11 +63,19 @@ class DCNV2_implicit(torch.nn.Module):
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
         embed_x = self.embedding(x).view(-1, self.embed_output_dim)
-        x_l1 = self.cn(embed_x)
-        h_l2 = self.mlp(embed_x)
-        x_stack = torch.cat([x_l1, h_l2], dim=1)
-        p = self.linear(x_stack)
-        output = torch.sigmoid(p.squeeze(1))
+
+        if self.use_stacking:
+            x_l = self.cn(embed_x)
+            h_l = self.mlp(x_l)
+            output = torch.sigmoid(h_l.squeeze(1))
+
+        else:
+            x_l1 = self.cn(embed_x)
+            h_l2 = self.mlp(embed_x)
+            x_stack = torch.cat([x_l1, h_l2], dim=1)
+            p = self.linear(x_stack)
+            output = torch.sigmoid(p.squeeze(1))
+
         return output
 
     def fit(self):
@@ -133,7 +148,6 @@ class DCNV2_implicit(torch.nn.Module):
         self.load_state_dict(state_dict)
 
 class MultiLayerPerceptron(torch.nn.Module):
-
     def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
         super().__init__()
         layers = list()
